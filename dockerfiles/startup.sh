0ce080a3d5a930dff8a3cf66c916a830de6cf896
rfraposa <richraposa@gmail.com>
2017-04-20 18:20:01 -0600

    created startup.sh

--- dockerfiles/startup.sh
+++ dockerfiles/startup.sh
@@ -1,1 +1,1 @@
-sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -dns 127.0.0.1 -p 22 -name host1 -h host1 -d --privileged=true  -i -t rich/ece_node
+sudo docker run -v /var/run/docker.sock:/var/run/docker.sock --dns 8.8.8.8 -p 22 --name host1 -h host1 -d --privileged=true  -i -t rich/ece_node
