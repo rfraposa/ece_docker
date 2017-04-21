e4e3bdcbcbfe1dfd61b1dead39fd7c3c626af8da
rfraposa <richraposa@gmail.com>
2017-04-20 18:42:42 -0600

    starts up 3 nodes

--- dockerfiles/startup.sh
+++ dockerfiles/startup.sh
@@ -1,1 +1,7 @@
-sudo docker run -v /var/run/docker.sock:/var/run/docker.sock --dns 8.8.8.8 -p 22 --name host1 -h host1 -d --privileged=true  -i -t rich/ece_node
+for i in {1..3}
+do
+    echo "output: $i"
+    sudo docker run -v /var/run/docker.sock:/var/run/docker.sock --dns 8.8.8.8 -p 22 --name host$i -h host$i -d --privileged=true  -i -t rich/ece_node
+    IP_node=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" host$i)
+    echo "host$i Server started at $IP_node"
+done
