40adbbb387d5eb74abe467b75766d7ef2ec30d26
rfraposa <richraposa@gmail.com>
2017-04-20 20:17:23 -0600

    added memory to docker run command

--- dockerfiles/startup.sh
+++ dockerfiles/startup.sh
@@ -1,7 +1,7 @@
 for i in {1..3}
 do
     echo "output: $i"
-    sudo docker run -v /var/run/docker.sock:/var/run/docker.sock --memory=8192 --dns 8.8.8.8 -p 22 --name host$i -h host$i -d --privileged  -i -t rich/ece_node
+    sudo docker run -v /var/run/docker.sock:/var/run/docker.sock --memory=8192m --dns 8.8.8.8 -p 22 --name host$i -h host$i -d --privileged  -i -t rich/ece_node
     IP_node=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" host$i)
     echo "host$i Server started at $IP_node"
 done
